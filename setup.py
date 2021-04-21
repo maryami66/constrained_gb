65d281df97a69d32f6bfd3b011e314ec56ecebe2
maryami66 <maryami_66@yahoo.com>
2021-04-21 13:40:39 +0200

    change licence to GNU

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+#  Copyright (c) 2020 Robert Bosch GmbH
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from setuptools import setup
 from os import path
 
@@ -28,7 +44,7 @@ setup(
     keywords='constrained optimization, gradient boosting',
     classifiers=[
         "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: GNU General Public License (GPLv3)",
+        "License :: OSI Approved :: GNU",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.6'
